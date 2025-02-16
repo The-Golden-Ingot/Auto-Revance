ec2ae1ff0bf9531601a16f3e9a7551677e0d213c
The-Golden-Ingot <michel.svenberg@gmail.com>
2025-02-16 17:56:56 +0100

    Simplify Lightroom version URL extraction using data-url attribute
    - Update CSS selector to directly extract version URL using data-url
    - Remove sed-based string manipulation for URL extraction
    - Maintain existing download workflow with more direct attribute targeting

--- src/build/Revanced-Beta.sh
+++ src/build/Revanced-Beta.sh
@@ -28,8 +28,8 @@ patch_lightroom() {
 	# Get versions page directly
 	versions_page=$(req "https://adobe-lightroom-mobile.en.uptodown.com/android/versions" -)
 	
-	# Extract first APK version URL using combined selector
-	version_url=$(echo "$versions_page" | $pup '.versions .content div.type.apk:nth-of-type(1) attr{onclick}' | sed -n "s/.*'\(https[^']*\)'.*/\1/p")
+	# Extract first APK version URL using data-url attribute
+	version_url=$(echo "$versions_page" | $pup '.versions .content div.type.apk:nth-of-type(1) attr{data-url}')
 	
 	if [ -z "$version_url" ]; then
 		echo "Failed to extract version URL"
