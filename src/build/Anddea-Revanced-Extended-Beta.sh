81292486c09fc50f2c0c513de87374267725a293
The-Golden-Ingot <michel.svenberg@gmail.com>
2025-02-16 19:25:09 +0100

    Optimize app patching for specific architectures
    - Modify YouTube Beta script to patch only Arm64-v8a architecture
    - Update Google Photos Beta script to patch only Arm64-v8a
    - Streamline SoundCloud Beta patching to focus on Arm64-v8a architecture
    - Remove redundant architecture-specific patching steps

--- src/build/Anddea-Revanced-Extended-Beta.sh
+++ src/build/Anddea-Revanced-Extended-Beta.sh
@@ -12,11 +12,9 @@ patch_youtube_rve() {
     get_apk "com.google.android.youtube" "youtube-beta" "youtube" "google-inc/youtube/youtube"
     patch "youtube-beta" "anddea" "inotia"
 
-    # Split architecture Youtube:
+    # Patch YouTube Arm64-v8a only:
     get_patches_key "youtube-rve-anddea"
-    for i in {0..3}; do
-        split_arch "youtube-beta" "anddea" "$(gen_rip_libs ${libs[i]})"
-    done
+    split_arch "youtube-beta" "anddea" "$(gen_rip_libs armeabi-v7a x86_64 x86)"
 }
 
 case "$1" in
--- src/build/Revanced-Beta.sh
+++ src/build/Revanced-Beta.sh
@@ -9,15 +9,10 @@ revanced_dl(){
 
 patch_ggphotos() {
 	revanced_dl
-	# Patch Google photos:
-	# Arm64-v8a
+	# Patch Google photos (Arm64-v8a only):
 	get_patches_key "gg-photos"
 	get_apk "com.google.android.apps.photos" "gg-photos-arm64-v8a-beta" "photos" "google-inc/photos/google-photos" "arm64-v8a" "nodpi"
 	patch "gg-photos-arm64-v8a-beta" "revanced"
-	# Armeabi-v7a
-	get_patches_key "gg-photos"
-	get_apk "com.google.android.apps.photos" "gg-photos-armeabi-v7a-beta" "photos" "google-inc/photos/google-photos" "armeabi-v7a" "nodpi"
-	patch "gg-photos-armeabi-v7a-beta" "revanced"
 }
 
 patch_lightroom() {
@@ -66,11 +61,11 @@ patch_lightroom() {
 
 patch_soundcloud() {
 	revanced_dl
-	# Patch SoundCloud:
+	# Patch SoundCloud (Arm64-v8a only):
 	get_patches_key "soundcloud"
 	get_apk "com.soundcloud.android" "soundcloud-beta" "soundcloud-soundcloud" "soundcloud/soundcloud-soundcloud/soundcloud-play-music-songs" "Bundle_extract"
-	split_editor "soundcloud-beta" "soundcloud-beta"
-	patch "soundcloud-beta" "revanced"
+	split_editor "soundcloud-beta" "soundcloud-arm64-v8a-beta" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64"
+	patch "soundcloud-arm64-v8a-beta" "revanced"
 }
 
 case "$1" in
