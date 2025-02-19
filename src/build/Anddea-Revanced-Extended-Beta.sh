746c4f56dfd5b09d5c6b2c6180887d0782b9f0c2
The-Golden-Ingot <michel.svenberg@gmail.com>
2025-02-19 21:46:58 +0100

    Refactor YouTube RVE build script to use dynamic library removal
    - Implement `gen_rip_libs` function to generate library removal arguments
    - Simplify architecture filtering for YouTube RVE build
    - Remove hardcoded split configuration in favor of dynamic library removal

--- src/build/Anddea-Revanced-Extended-Beta.sh
+++ src/build/Anddea-Revanced-Extended-Beta.sh
@@ -7,11 +7,11 @@ patch_youtube_rve() {
     dl_gh "revanced-patches" "anddea" "prerelease"
     dl_gh "revanced-cli" "inotia00" "latest"
 
-    # Patch YouTube (Arm64-v8a only, but don't include in name):
     get_patches_key "youtube-rve-anddea"
     get_apk "com.google.android.youtube" "youtube" "youtube" "google-inc/youtube/youtube"
-    split_editor "youtube" "youtube" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64"
-    patch "youtube" "anddea" "inotia"
+    # Generate rip-lib arguments to remove non-arm64-v8a architectures
+    rip_libs=$(gen_rip_libs "armeabi-v7a" "x86" "x86_64")
+    patch "youtube" "anddea" "inotia" "$rip_libs"
 }
 
 case "$1" in
