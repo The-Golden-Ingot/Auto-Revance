f3b8650399ff84ed6617b71d36c427b357a41314
The-Golden-Ingot <michel.svenberg@gmail.com>
2025-02-19 22:05:11 +0100

    Refactor YouTube RVE build script to use dynamic library removal with `gen_rip_libs`
    - Remove "Bundle_extract" option from APK download
    - Use `gen_rip_libs` to dynamically generate library removal arguments
    - Simplify architecture filtering for YouTube RVE build

--- src/build/Anddea-Revanced-Extended-Beta.sh
+++ src/build/Anddea-Revanced-Extended-Beta.sh
@@ -9,10 +9,10 @@ patch_youtube_rve() {
 
     # Patch YouTube (Arm64-v8a only, but don't include in name):
     get_patches_key "youtube-rve-anddea"
-    get_apk "com.google.android.youtube" "youtube" "youtube" "google-inc/youtube/youtube" "Bundle_extract"
-    # Only build arm64-v8a version and remove unnecessary DPI resources
-    split_editor "youtube" "youtube" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64 split_config.mdpi split_config.hdpi split_config.xhdpi split_config.xxhdpi split_config.tvdpi"
-    patch "youtube" "anddea" "inotia"
+    get_apk "com.google.android.youtube" "youtube" "youtube" "google-inc/youtube/youtube"
+    # Generate rip-lib arguments to remove non-arm64-v8a architectures
+    rip_libs=$(gen_rip_libs "armeabi-v7a" "x86" "x86_64")
+    patch "youtube" "anddea" "inotia" "$rip_libs"
 }
 
 case "$1" in
